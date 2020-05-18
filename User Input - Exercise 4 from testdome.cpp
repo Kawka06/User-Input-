eaa765bafaa73a2f9f2650af32bc80fe3726f7d5
Kawka-6 <kawka.mateusz1990606@gmail.com>
2020-05-18 22:53:43 +0200

    Dodaj pliki projektów.

---
+++ User Input - Exercise 4 from testdome.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+class TextInput
+{
+public:
+
+    virtual ~TextInput() {}
+
+    virtual void add(char c) { data += c; }
+
+    std::string getValue() { return data; }
+
+private:
+
+    std::string data;
+};
+
+class NumericInput : public TextInput
+{
+    void add(char c)
+    {
+        if (('0' <= 'c') && (c <= '9'))
+            TextInput::add(c);
+    }
+};
+
+#ifndef RunTests
+int main()
+{
+    TextInput* input = new NumericInput();
+    input->add('8');
+    input->add('y');
+    input->add('9');
+    std::cout << input->getValue();
+
+}
+#endif
